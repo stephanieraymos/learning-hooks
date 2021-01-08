71751e8b7233311a85ae413d35dce9ff789ea92e
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-07 17:55:14 -0800

    Title changes in console but not on screen because comp is not being re-rendered

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 const ErrorExample = () => {
+  let title = "Title name"
+  const handleClick = () => {
+    title = "Hello People"
+  }
+  
+  return <React.Fragment>
+    <h2>{title}</h2>
+    <button type="button" className="btn" onClick={handleClick}>
+      Change title
+    </button>
+  </React.Fragment>
   return <h2>useState error example</h2>;
 };
 
