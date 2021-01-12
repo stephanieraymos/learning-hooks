5c0fe99ba7360ab03378792c795a5b8e3a46cc0c
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-11 19:57:42 -0800

    useState

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
+// useState is techincally a function
+
 
 const UseStateBasics = () => {
+
+  const [text, setText] = useState('random title') //second value is function, useState perameter is default value
   return <h2>useState basic example</h2>;
 };
 
