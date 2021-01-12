1307c1c8c42f8c8c815c4a4456a07085901cdf9a
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-11 20:02:37 -0800

    Change text button

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -5,9 +5,15 @@ import React, { useState } from 'react';
 const UseStateBasics = () => {
 
   const [text, setText] = useState('random title') //second value is function, useState perameter is default value
+
+  const handleClick = () => {
+    setText('random text changed');
+  }
+
   return (
     <React.Fragment>
       <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>Change Title</button>
     </React.Fragment>
   )
 };
