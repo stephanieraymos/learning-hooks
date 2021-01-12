5a54f2bf26580278d7c3eff2aec09b7acf359261
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-11 20:24:53 -0800

    Person

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/2-useState-basics';
+import Setup from './tutorial/1-useState/setup/3-useState-array';
 
 
 function App() {
   return (
     <div className='container'>
-      <h2>Birthday Reminder</h2>
       <Setup />
     </div>
   )
