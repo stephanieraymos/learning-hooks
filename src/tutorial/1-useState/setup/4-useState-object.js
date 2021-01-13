76de22533fc7c1b8d9137f76438058cf88d134ee
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-12 20:06:42 -0800

    Using object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -8,7 +8,9 @@ const UseStateObject = () => {
   })
   return(
     <>
-      <h1>Hello, {person.name}</h1>
+      <h1>Hello, {person.name}!</h1>
+      <h1>You are {person.age} years old.</h1>
+      <h1>{person.message}</h1>
     </>
   )
 };
