98401ed4fae86710485591dd88fccd63fcdc6459
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-13 18:22:21 -0800

    Value

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
+  const [value, setValue] = useState(0)
   return(
     <>
     <section style={{margin: '4rem 0'}}>
       <h2>Regular Counter</h2>
+      <h1>{value}</h1>
     </section>
     </>
   )
