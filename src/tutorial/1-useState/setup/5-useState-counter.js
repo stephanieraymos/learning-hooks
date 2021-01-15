14dca89dd8a2c5d7837a62f8c497536394f378a3
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-14 19:06:16 -0800

    Keeping regular example

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -9,11 +9,19 @@ const UseStateCounter = () => {
 
   return(
     <>
-
+    
+    <section style={{margin: '4rem 0'}}>
+      <h2>Regular Counter</h2>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value -1)}>Decrease</button>
+      <button className="btn" onClick={reset}>Reset</button>
+      <button className="btn" onClick={() => setValue(value +1)}>Increase</button>
+    </section>
+    
     <section style={{margin: '4rem 0'}}>
       <h2>Complex Counter</h2>
       <h1>{value}</h1>
-      <button className="btn" >Increase Later</button>
+      <button className="btn" onClick={complexIncrease}>Increase Later</button>
     </section>
 
     </>
