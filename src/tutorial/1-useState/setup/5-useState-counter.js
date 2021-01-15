fedb9241ae6eaff368120a21b2b15ca0ee54ec92
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-14 19:22:08 -0800

    Final comment

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -12,7 +12,7 @@ const UseStateCounter = () => {
       // setValue(value + 1) This way is async so it kept the value that was originally there (0) and would only increase one no matter how many times clicked in 2 seconds, this is because it was only increasing +1 from 0.
       setValue((prevState) => {
         return prevState + 1; 
-      }) //This is the fix, calling the function within the function; before the timeout. Here we are getting the current state/value before the update.
+      }) //This is the fix, calling the function within the function; before the timeout. Here we are getting the current state/value before the update (the value previous to the update.)
     }, 2000); //2 seconds
   }
 
