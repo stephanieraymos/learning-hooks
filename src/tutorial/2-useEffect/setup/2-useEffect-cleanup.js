d371ce1cc8acc38dd62d4bc014d119806983d0a3
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-17 20:23:59 -0800

    Event listener on window + checkSize function

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -6,11 +6,19 @@ import React, { useState, useEffect } from 'react';
 const UseEffectCleanup = () => {
 
   const [size, setSize] = useState(window.innerWidth)
-  console.log(size)
+
+  checkSize = () => {
+    setSize(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', checkSize)
+  })
+
   return(
     <>
     <h1>Window</h1>
-    <h2>{size}</h2>
+    <h2>{size} px</h2>
     </>
   );
 };
