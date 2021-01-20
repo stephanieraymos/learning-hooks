9d82d541603ddca13314efe1d285b66386cf62f0
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-19 18:07:48 -0800

    Changed comment

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -6,7 +6,7 @@ const ShortCircuit = () => {
   const [text, setText] = useState('')
   const firstValue = text || 'hello world';
   //If first value is false, then it returns the second value, if true then it returns the intial value of 'text' which is the empty string.
-  const secondValue = text && 'hello world'; //If first value (text) is true; then it returns the second value (hellow world), if false then it returns the first value of string; empty string.
+  const secondValue = text && 'hello world'; //If first value (text) is true; then it returns the second value (hellow world), if false then it returns nothing.
 
   return <>
   <h1>{text || 'john doe'}</h1>
