199e8e651fd796cb61aea32ccc405f959188aee9
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-19 16:54:41 -0800

    Getting users

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -4,9 +4,22 @@ const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
   const [users, setUsers] = useState([]);
+
+  const getUsers = async() => {
+    //reponse it waiting for data to be fetched from the url declared on line 3
+    const response = await fetch(url);
+    
+    const users = await response.json();
+    console.log(users);
+  }
+  //USE EFFECT CAN'T BE ASYNC: IT CAN'T RETURN A PROMISE,and async returns a promise. This is why I created a seperate fetch function above to call inside of useEffect; so that it could be async.
+  // This function could have been set up INSIDE useEffect, or as a serpate function as I have done.
+  useEffect(() => {
+    
+  })
   return (
     <>
-      <h3>githib users</h3>
+      <h3>github users</h3>
     </>
   );
 };
