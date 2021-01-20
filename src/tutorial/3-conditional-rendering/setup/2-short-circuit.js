464590510d546b7efda97066f328dd6779cd2ec0
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-19 18:52:46 -0800

    Toggling error

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,17 +4,18 @@ import React, { useState } from 'react';
 
 const ShortCircuit = () => {
   const [text, setText] = useState('')
-  const firstValue = text || 'hello world';
-  //If first value is false, then it returns the second value, if true then it returns the intial value of 'text' which is the empty string.
-  const secondValue = text && 'hello world'; //If first value (text) is true; then it returns the second value (hellow world), if false then it returns nothing.
+  const [isError, setIsError] = useState(false)
+
+  // const firstValue = text || 'hello world';
+  // //If first value is false, then it returns the second value, if true then it returns the intial value of 'text' which is the empty string.
+  // const secondValue = text && 'hello world'; //If first value (text) is true; then it returns the second value (hellow world), if false then it returns nothing.
 
   return <>
   <h1>{text || 'john doe'}</h1>
   {/* || is saying "is that <-- false, okay then do this -->"
   && is saying "is that <-- true, okay then do this -->" */}
-  {text && <h1>hello world</h1>}
-  {!text && <h1>hello world</h1>}
-  {/* ^This is the same as the first h1 */}
+  <button className="btn" onClick={() => setIsError(!isError)}>toggle error</button>
+  {isError && <h1>Error...</h1>}
   </>;
 };
 
