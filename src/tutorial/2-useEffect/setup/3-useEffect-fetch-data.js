595ba31b00a2f33db67db43bd2727eaf4f9c2489
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-19 17:04:24 -0800

    Starting to return the data, made sure to set up [] as second argument for useEffect to avoid endless loop

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
+  //useState perserves value and triggers re-render once any part of this state is updated
   const [users, setUsers] = useState([]);
 
   const getUsers = async() => {
@@ -10,16 +11,30 @@ const UseEffectFetchData = () => {
     const response = await fetch(url);
     
     const users = await response.json();
-    console.log(users);
+
+    setUsers(users)
+    // console.log(users);
   }
   //USE EFFECT CAN'T BE ASYNC: IT CAN'T RETURN A PROMISE,and async returns a promise. This is why I created a seperate fetch function above to call inside of useEffect; so that it could be async.
   // This function could have been set up INSIDE useEffect, or as a serpate function as I have done.
   useEffect(() => {
     getUsers();
-  })
+  }, [])
   return (
     <>
       <h3>github users</h3>
+
+      <ul className="users">
+      {users.map((user) => {
+        const {id, login, avatar_url, html_url} = user
+        return <li key={id}>
+          <img src={avatar_url} alt={login} />
+          <div>
+            <h4>{login}</h4>
+          </div>
+        </li>
+      })}
+      </ul>
     </>
   );
 };
