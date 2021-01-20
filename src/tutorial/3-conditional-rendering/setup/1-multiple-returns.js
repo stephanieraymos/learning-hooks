126abc7cd0175d6a97ec97eee3c5790f1c427afc
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-19 17:16:36 -0800

    Setup state

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState('default user');
+
 
   if(loading) {
     return <h2>Loading...</h2>
   } 
+
   return <h2>multiple returns</h2>;
 };
 
