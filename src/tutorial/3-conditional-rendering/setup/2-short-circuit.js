03a9375a16dd7457a8b135b746c6f8bff908a91a
Stephanie Raymos <stephanieraymos@gmail.com>
2021-01-20 18:37:16 -0800

    Paranthesis + formatting

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,26 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  const [text, setText] = useState('')
-  const [isError, setIsError] = useState(false)
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
 
   // const firstValue = text || 'hello world';
   // //If first value is false, then it returns the second value, if true then it returns the intial value of 'text' which is the empty string.
   // const secondValue = text && 'hello world'; //If first value (text) is true; then it returns the second value (hellow world), if false then it returns nothing.
 
-  return <>
-    <h1>{text || 'john doe'}</h1>
-    {/* || is saying "is that <-- false, okay then do this -->"
+  return (
+    <>
+      <h1>{text || "john doe"}</h1>
+      {/* || is saying "is that <-- false, okay then do this -->"
   && is saying "is that <-- true, okay then do this -->" */}
-    <button className="btn" onClick={() => setIsError(!isError)}>toggle error</button>
-    {/* !isError is the opposite of isError, so every time it is clicked it is setting isError to the opposite of what it currently is; toggle. */}
-    {isError && <h1>Error...</h1>}
-    {isError ? <p>There is an error</p> : <div>
-      <h2>There is no error</h2>
-    </div>}
-  </>;
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {/* !isError is the opposite of isError, so every time it is clicked it is setting isError to the opposite of what it currently is; toggle. */}
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>There is an error</p>
+      ) : (
+          <div>
+            <h2>There is no error</h2>
+          </div>
+        )}
+    </>
+  );
 };
 
 export default ShortCircuit;
