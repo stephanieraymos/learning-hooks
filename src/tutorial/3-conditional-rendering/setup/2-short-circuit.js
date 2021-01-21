ec76b3ab40449c5f6e913c870af4f0f05b36184a
Stephanie Raymos <stephanieraymos@gmail.com>
2021-01-20 18:34:35 -0800

    Ternary operator

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -11,12 +11,15 @@ const ShortCircuit = () => {
   // const secondValue = text && 'hello world'; //If first value (text) is true; then it returns the second value (hellow world), if false then it returns nothing.
 
   return <>
-  <h1>{text || 'john doe'}</h1>
-  {/* || is saying "is that <-- false, okay then do this -->"
+    <h1>{text || 'john doe'}</h1>
+    {/* || is saying "is that <-- false, okay then do this -->"
   && is saying "is that <-- true, okay then do this -->" */}
-  <button className="btn" onClick={() => setIsError(!isError)}>toggle error</button>
-  {/* !isError is the opposite of isError, so every time it is clicked it is setting isError to the opposite of what it currently is; toggle. */}
-  {isError && <h1>Error...</h1>}
+    <button className="btn" onClick={() => setIsError(!isError)}>toggle error</button>
+    {/* !isError is the opposite of isError, so every time it is clicked it is setting isError to the opposite of what it currently is; toggle. */}
+    {isError && <h1>Error...</h1>}
+    {isError ? <p>There is an error</p> : <div>
+      <h2>There is no error</h2>
+    </div>}
   </>;
 };
 
