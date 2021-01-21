1982fc1a6ef552a52e7cb0d9581bccb5dd13aebb
Stephanie Raymos <stephanieraymos@gmail.com>
2021-01-20 18:41:05 -0800

    State + button with onClick to toggle

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+
+  return <>
+    <button className="btn" onClick={() => setShow(!show)}>Show/Hide</button>
+  </>;
 };
 
 export default ShowHide;
