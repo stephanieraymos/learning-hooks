20c1eca6a1e55351bbec002be4ccdeebfb965045
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-26 17:59:55 -0800

    onChange

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -23,13 +23,13 @@ const ControlledInputs = () => {
             <label htmlFor="firstName">
               Name: 
             </label>
-            <input type="text" id="firstName" name="firstName" value={firstName}/>
+            <input type="text" id="firstName" name="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
           </div>
           <div className="form-control">
             <label htmlFor="email">
               Email: 
             </label>
-            <input type="text" id="email" name="email" value={email}/>
+            <input type="text" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
           </div>
           <button type="submit">Add person</button>
         </form>
