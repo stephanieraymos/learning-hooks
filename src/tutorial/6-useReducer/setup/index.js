261ddd953acb05b4be9ac5ca8e07bc1a058b0e5f
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-02-02 19:45:23 -0800

    State for people + modal

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,6 +4,9 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+  
   return <h2>useReducer</h2>;
 };
 
