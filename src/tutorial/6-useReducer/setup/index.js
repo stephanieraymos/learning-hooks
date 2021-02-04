28bb171d8ec3686d154a0e15a0a68365438fbb2f
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-02-03 20:29:35 -0800

    Reducer function

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -3,10 +3,14 @@ import Modal from "./Modal";
 import { data } from "../../../data";
 // reducer function
 
+const reducer = (state, action) => {
+
+}
+
 const Index = () => {
   const [name, setName] = useState("");
   const [state, dispatch] = useReducer(reducer)
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
