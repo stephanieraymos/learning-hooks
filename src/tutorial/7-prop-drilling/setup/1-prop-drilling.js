927a7f0c350892f8326f42273a1e91bcbbf6bb04
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-01 14:51:22 -0800

    Remove button working

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -7,28 +7,36 @@ import { data } from "../../../data";
 const PropDrilling = () => {
   const [people, setPeople] = useState(data);
 
+  const removePerson = (id) => {
+    setPeople((people) => {
+      //Return all people whose id doesn't match the id passed in (the removed person)
+      return people.filter((person) => person.id !== id); //If person id doesn't match the (removed) id passed into the function; return them, if not (that's the id I'm deleting;) then remove it.
+    });
+  };
+
   return (
     <section>
       <h3>Prop Drilling</h3>
-      <List people={people} />
+      <List people={people} removePerson={removePerson}/>
     </section>
   );
 };
 
-const List = ({ people }) => {
+const List = ({ people, removePerson }) => {
   return (
     <>
       {people.map((person) => {
-        return <SinglePerson key={person.id} />;
+        return <SinglePerson key={person.id} {...person} removePerson={removePerson} />;
       })}
     </>
   );
 };
 
-const SinglePerson = ({ id, name }) => {
+const SinglePerson = ({ id, name, removePerson }) => {
   return (
     <div className="item">
-      <h4>Single Item</h4>
+      <h4>{name}</h4>
+      <button onClick={() => removePerson(id)}>Remove</button>
     </div>
   );
 };
